dbb1a23f38ddaa87ed9ddc6478c2374a2c6f1ec5
mikepxq <wanpdjb@gmail.com>
2022-12-28 21:33:34 +0800

    feat: fvm flutter

--- 项目记录.dart
+++ 项目记录.dart
@@ -1,19 +1,18 @@
 /**
-1.初始项目
-  跳过环境配置
-  https://flutter.cn/docs/get-started/install/macos
-  flutter create flutter_template
-  用xcode打开 ios/Runner.xcworkspace
-  然后 运行调试
-2.环境
-  单版本全局环境变量 .zshrc
-  export PATH="$HOME/work/env/flutter/bin:$PATH"
-  2.1 fvm 版本管理器
+1.环境配置
+  1.1. fvm 版本管理器
     brew tap leoafarias/fvm
     brew install fvm
-  2.2 .zshrc
-    export PUB_HOSTED_URL=http://mirrors.cnnic.cn/dart-pub
-    export FLUTTER_STORAGE_BASE_URL=http://mirrors.cnnic.cn/flutter 
+   1.2. .zshrc
+    // export PUB_HOSTED_URL=http://mirrors.cnnic.cn/dart-pub
+    // export FLUTTER_STORAGE_BASE_URL=http://mirrors.cnnic.cn/flutter 
     export FVM_DIR="$HOME/.fvm"
-    source "/usr/local/opt/fvm/init.sh"
+    // source "/usr/local/opt/fvm/init.sh"
+2.初始项目
+  参照：https://flutter.cn/docs/get-started/install/macos
+  flutter create flutter_template
+  用xcode打开 ios/Runner.xcworkspace
+
+
+
 */

db86245f8c0ba71ab003b25a42e4ab1c87ae1550
mikepxq <wanpdjb@gmail.com>
2023-01-11 23:30:57 +0800

    feat: 简单路由

--- 项目记录.dart
+++ 项目记录.dart
@@ -1,17 +1,34 @@
+/**单独记录环境配置
+1. fvm 版本管理器
+  brew tap leoafarias/fvm
+  brew install fvm
+2. .zshrc
+  // export PUB_HOSTED_URL=http://mirrors.cnnic.cn/dart-pub
+  // export FLUTTER_STORAGE_BASE_URL=http://mirrors.cnnic.cn/flutter 
+  export FVM_DIR="$HOME/.fvm"
+  // source "/usr/local/opt/fvm/init.sh"
+ */
+
 /**
-1.环境配置
-  1.1. fvm 版本管理器
-    brew tap leoafarias/fvm
-    brew install fvm
-   1.2. .zshrc
-    // export PUB_HOSTED_URL=http://mirrors.cnnic.cn/dart-pub
-    // export FLUTTER_STORAGE_BASE_URL=http://mirrors.cnnic.cn/flutter 
-    export FVM_DIR="$HOME/.fvm"
-    // source "/usr/local/opt/fvm/init.sh"
-2.初始项目
+1.初始项目
   参照：https://flutter.cn/docs/get-started/install/macos
-  flutter create flutter_template
-  用xcode打开 ios/Runner.xcworkspace
+    flutter create flutter_template
+    用xcode打开 ios/Runner.xcworkspace
+2.统一环境
+  flutter 项目默认代码开发环境比较一致，暂无需处理
+3.结构说明
+  apis
+  参考：https://pub.flutter-io.cn/packages/http/install
+    fvm dart pub add http
+  assets
+  screens
+  store
+4.ui
+5.基础功能实现
+5.1. 权限管理
+  需要 token 持续化存储
+
+
 
 
 

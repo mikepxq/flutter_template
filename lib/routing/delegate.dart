4c7818133cd013cd68b5f215b1b4c6b9d665c0fa
mikepxq <wanpdjb@gmail.com>
2023-03-28 23:34:48 +0800

    feat: 迁移来自项目 && 可运行

--- lib/routing/delegate.dart
+++ lib/routing/delegate.dart
@@ -1,4 +1,12 @@
+// Copyright 2021, the Flutter project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
+
 import 'parsed_route.dart';
 import 'route_state.dart';
 
@@ -10,19 +18,21 @@ class SimpleRouterDelegate extends RouterDelegate<ParsedRoute>
   @override
   final GlobalKey<NavigatorState> navigatorKey;
 
-  SimpleRouterDelegate(
-      {required this.routeState,
-      required this.builder,
-      required this.navigatorKey}) {
+  SimpleRouterDelegate({
+    required this.routeState,
+    required this.builder,
+    required this.navigatorKey,
+  }) {
     routeState.addListener(notifyListeners);
   }
 
   @override
-  Widget build(BuildContext context) => build(context);
+  Widget build(BuildContext context) => builder(context);
 
   @override
   Future<void> setNewRoutePath(ParsedRoute configuration) async {
     routeState.route = configuration;
+    return SynchronousFuture(null);
   }
 
   @override

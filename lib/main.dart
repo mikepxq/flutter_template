fbb590f33f2267ff36feda36e7781fae50e510ec
mikepxq <wanpdjb@gmail.com>
2023-03-28 23:06:07 +0800

    feat: 暂停copy 无法运行

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,42 @@
 import 'dart:io';
 
-import 'package:flutter/foundation.dart';
+// import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_template/screens/home.dart';
-import 'package:flutter_template/screens/login.dart';
+import 'package:flutter_template/app.dart';
+// import 'package:flutter_template/screens/home.dart';
+// import 'package:flutter_template/screens/login.dart';
 import 'package:url_strategy/url_strategy.dart';
 
 void main() {
   //web hash 路由
   setHashUrlStrategy();
-  runApp(const MyApp());
+  runApp(const Bookstore());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      routes: {
-        '/': (context) => const Home(),
-        '/login': (context) => const Login(),
-      },
-    );
-  }
-}
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Template',
+//       theme: ThemeData(
+//         // This is the theme of your application.
+//         //
+//         // Try running your application with "flutter run". You'll see the
+//         // application has a blue toolbar. Then, without quitting the app, try
+//         // changing the primarySwatch below to Colors.green and then invoke
+//         // "hot reload" (press "r" in the console where you ran "flutter run",
+//         // or simply save your changes to "hot reload" in a Flutter IDE).
+//         // Notice that the counter didn't reset back to zero; the application
+//         // is not restarted.
+//         primarySwatch: Colors.blue,
+//       ),
+//       routes: {
+//         '/': (context) => const Home(),
+//         '/login': (context) => const Login(),
+//       },
+//     );
+//   }
+// }

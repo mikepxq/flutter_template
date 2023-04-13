9bfbdb958ddf88022fd94c5c9020a3793e30d87d
kovic <kovic@ttostech.com01~>
2023-04-13 19:16:20 +0800

    feat: 路由 ing

---
+++ lib/src/routing/index.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class RouteStateScope extends InheritedNotifier {
+// TODO 路由
+  const RouteStateScope(
+      {required super.notifier, required super.child, super.key});
+}
--- 项目记录.dart
+++ 项目记录.dart
@@ -1,18 +1,21 @@
 /**
-1.环境配置
-  1.1. fvm 版本管理器
-    brew tap leoafarias/fvm
-    brew install fvm
-   1.2. .zshrc
-    // export PUB_HOSTED_URL=http://mirrors.cnnic.cn/dart-pub
-    // export FLUTTER_STORAGE_BASE_URL=http://mirrors.cnnic.cn/flutter 
-    export FVM_DIR="$HOME/.fvm"
-    // source "/usr/local/opt/fvm/init.sh"
-2.初始项目
-  参照：https://flutter.cn/docs/get-started/install/macos
-  flutter create flutter_template
-  用xcode打开 ios/Runner.xcworkspace
+1.初始化
+2.格式化
+3.结构
+  lib
+    router 
+      哈希路由
+      fvm flutter pub add url_strategy
+
 
 
 
 */
+/**参照:
+  1.https://pub.dev/
+
+ */
+/**命令:
+  1. fvm flutter pub add  <>
+ 
+ */
